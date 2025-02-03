8992c281383e2ca5af6d005e628006a474cfe91d
Amit2384 <chandajkaramit@gmail.com>
2025-02-03 23:59:34 +0530

    Update script.js

--- resume/script.js
+++ resume/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const toggleSkillsButton = document.getElementById('toggleSkills');
   const skillsList = document.getElementById('skillsList');
+  const downloadPdfButton = document.getElementById('downloadPdf');
 
   toggleSkillsButton.addEventListener('click', function() {
     if (skillsList.style.display === 'none') {
@@ -8,5 +9,15 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
       skillsList.style.display = 'none';
     }
+  });
+
+  downloadPdfButton.addEventListener('click', function() {
+        const pdfUrl = "resume.pdf";
+        const link = document.createElement('a');
+        link.href = pdfUrl;
+        link.download = "Amit_Chandajkar_resume.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
   });
 });
